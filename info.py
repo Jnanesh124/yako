6d0d88425063433e1cda4417433f33cacacfadfb
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-03-09 22:11:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ API_ID       = 29942004
 API_HASH     = "ad92f01e4a90cddebbea0ad16fa23026"
 BOT_TOKEN    = "6765313019:AAGYzdlzJ-w5TNM9npSbBg4sGyX-44K5uHA"
 SESSION      = "BQHI4PQAj44gGq6fA82kUkixQ3wb9i7yY_YRevL9DDnrzKfP4EqGNZhk-wB26felak-ODJ5qokBcVp9UwLzjM4Pe5nEK2-HxpDMIAGsmIo8H4K1YnpygfYNoz2CrDJ4zPbvLkygXTywAxHJMP6wGdKlhzu97SAbEpuIfxhBX2Zxtud0kR61FcfnRVAfYzSpgI6vMxtp-7zycekU2iU4lgvdWmsYptmdgXDRnKgugUvCt8dQ8V7xgV2wsZW7r_bDKHsy9AY5_a9n1TQCS6A7lZtlfazbBpeOySX6c2YadVS-W2358VIhdtuBx9SVFvdXY2NOIL_kFVBOXH7OT_5rqJ4fXjcPstwAAAABkI-HqAA"
-DATABASE_URI = "mongodb+srv://mangodb:mangodb@cluster0.dskkdky.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001997863812
 ADMIN        = 7099757942
