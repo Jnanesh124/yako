a9cc0f65b2fdfea83397dd558959acd6fefd92c5
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-07-04 19:14:52 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -36,7 +36,9 @@ async def search(bot, message):
           msg = await message.reply_text(text=head+results, disable_web_page_preview=True)
        _time = (int(time()) + (15*30))
        await save_dlt_message(msg, _time)
+       await results.message.delete(10)
     except:  
+        pass
        
 
 
