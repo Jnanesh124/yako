1fe4585a262d3579e03f66d9144f72b2b141bfdb
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-07-10 19:45:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
-API_ID       = 22231458
-API_HASH     = "f7cb57cc5f91c944f3ddeb8a60a99466"
+API_ID       = 21942125
+API_HASH     = "6d412af77ce89f5bb1ed8971589d61b5"
 BOT_TOKEN    = "6765313019:AAHYMI7W7VZbq5iqm_pPlnlN0_P8wbFjK1s"
-SESSION      = "BQFOz20AumoSUuDibX2penn2tIp5yHX_8U4BOL4qx2w2E6nH1dJyFDaplNMb7AGQPoDS_QKRUb6L5ntdOPkKD7U9Yy7am797nKcewbp_NwyRu_ZFWzr8gB1opssOcR2wwMalw6bRm1hzUTQNumHCBMRYqSnJNAB_66-PPkHQQHvxSHr9lwn5kRspvl7W2ZsxxRSb7kgydNMZtfuMjWqfmX-ce5lcSsrDjNkjxRGbK1u8SzzXKorXr9qYlGH-TJmcpU6EBKoXEDUmbhgyXKsOJqRilv4nhe6VEYw2pHIL4DTC1G1c-VKxy4BEKKmacmNCPXTW2bnDcV2gmtp2oiK-qpd2EZCDvAAAAABkI-HqAA"
-DATABASE_URI = "mongodb+srv://test:test@cluster0.ys8igld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+SESSION      = "1BVtsOLoBu7v5JwXLorbCo0xMomziZgJAcIZBAqW_JP4cIMgNsH5TS2d9gHp5FbSBnG0kWvKupJf_Bu0H0sSHiSYVr1rv3YWXRR2vWiqglKOEA8OQ21yczu1oiONb-_lIDZydiy5zsuj_civ4x5vBzXhUxjLGqpZYhSjYatVdbmiTTc27BJ0YMSCYHu_dMRJG6yhQl7qCD-BrN2nJwg7_leYyab3Dury8k-GNCpdnMqza5uo9h7EfjPE_b5IlENB7CHw_T9B1h-8s1sbJW_JO0tr3-p0BJ6cwuA57e1M6MaQMtCvaJzmx__345qEF3oMh333kWBLVkGAtT1lE_ghSqXV0Ti0aZ2g="
+DATABASE_URI = "mongodb+srv://strong:strong@cluster0.ix7usa3.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001997863812
 ADMIN        = 6177415112
