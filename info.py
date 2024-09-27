c6aab2263337093a02fc3d0aaae5f2540ff65747
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-09-27 20:15:13 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 21942125
 API_HASH     = "6d412af77ce89f5bb1ed8971589d61b5"
 BOT_TOKEN    = "6765313019:AAHYMI7W7VZbq5iqm_pPlnlN0_P8wbFjK1s"
-SESSION      = "BQFTOaIArcciQHoKsexkNEM4b3cEJBnPGHYrBN6tg2IY-wFT4DS2wtfcqaJTLrxr6A3VfJGpg7EWV3Nc9ntlViY17CGffvXf0mlQRRoNhj1pnbzoDSDUgU9bCqTvFgdcVcakqlfavz26HE2Obh_Mwir5Tjb4g9r03kmrYPo5XQwOY38vWAC7grAAPOm6XDsEBHq2wo1tM3hj_FL21QnaMXfzmAglBR83u5q2BTqOfFoDRZxxC9-NzYd_U46f01CEySKJZjEAcp5vw3MDFZlrdmrMU6-kUtqHjT2bQEgb-9Q7t9RpegjhymSAh23iw5woiCQ62qNovn6mJQhrAtNvwNfhvchR6QAAAAFwM9_IAA"
+SESSION      = "BQFTOaIAgYe3_KVCcT6L7KbSwmxlwA32zWmbYjedtAMABhkuKAyWJ5Lmxt0wu-WmzsMfzd-n4nfkQMpnYPK7_lguSLPeBGc_14LJP5nAxDiiflbFpY7opMJHCUBzxT-mm-BbxCupz5f2SmUtptONZ6jfnnzbEhd0ZYzKCFYvnYYvpj6g6q_kvFdI3pU9iE7UhpskCHJGBe6DThd5ZqWJzEx9JyfSNqNMkEHNCvmbV9zkH5Z3rtDK6mPSRkwrP3mGy5MrSiDPpEWhdBUPDRYgJlYbpcEyCvIGQdyhY0FqUCScOAFE_jbQOFWaUmQk-xhd-Ztkz5-wj5sCmPIxmNIyK-80s9K8HAAAAAFwM9_IAA"
 DATABASE_URI = "mongodb+srv://easyeasy740:easyeasy740@cluster0.1shrvws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1001997863812
 ADMIN        = 6331847574
