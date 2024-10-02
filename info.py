b217d54879173b7f2a04f05152fa28ea77e8323b
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-10-02 23:21:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ API_ID       = 21942125
 API_HASH     = "6d412af77ce89f5bb1ed8971589d61b5"
 BOT_TOKEN    = "6765313019:AAHYMI7W7VZbq5iqm_pPlnlN0_P8wbFjK1s"
 SESSION      = "BQFTOaIAgYe3_KVCcT6L7KbSwmxlwA32zWmbYjedtAMABhkuKAyWJ5Lmxt0wu-WmzsMfzd-n4nfkQMpnYPK7_lguSLPeBGc_14LJP5nAxDiiflbFpY7opMJHCUBzxT-mm-BbxCupz5f2SmUtptONZ6jfnnzbEhd0ZYzKCFYvnYYvpj6g6q_kvFdI3pU9iE7UhpskCHJGBe6DThd5ZqWJzEx9JyfSNqNMkEHNCvmbV9zkH5Z3rtDK6mPSRkwrP3mGy5MrSiDPpEWhdBUPDRYgJlYbpcEyCvIGQdyhY0FqUCScOAFE_jbQOFWaUmQk-xhd-Ztkz5-wj5sCmPIxmNIyK-80s9K8HAAAAAFwM9_IAA"
-DATABASE_URI = "mongodb+srv://easyeasy740:easyeasy740@cluster0.1shrvws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URI = "mongodb+srv://strong:strong@cluster0.ix7usa3.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001997863812
 ADMIN        = 6331847574
