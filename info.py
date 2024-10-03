d1071f96f4e0d4901cde4e5b1112a8eddff3b949
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-10-03 21:54:15 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
-API_ID       = 21942125
-API_HASH     = "6d412af77ce89f5bb1ed8971589d61b5"
+API_ID       = 29942004
+API_HASH     = "ad92f01e4a90cddebbea0ad16fa23026"
 BOT_TOKEN    = "6765313019:AAHYMI7W7VZbq5iqm_pPlnlN0_P8wbFjK1s"
 SESSION      = "BQHI4PQATVv5uF-p0ztSfnI2sjOk55qIZUIZ4kqSOBBZEr5ikUJGO-ngeMAMkdYVFcezIjxKZnz8OM0ZkF7oku0qbkqE_yukTp734ClmlqtbO6JqFtsJm2j5bVSK1iNZQxAo5Qyf-IlJydLBKYE8y-PmWzukYlJaGk5quYd2E7dPTnU9cv3bF14WBaCb8L2Sow_CP5E3rZcDzmJKiOcETLTuctXuTJTF2llZicgxMi-swnganyK08zf4vIzCi0mvX_rPLBYnUH78a_4jVUep7pA4ojWtcTlPLVVUm2N8nsmmmMH9d1cteDhRmApRHJmc6Q_GdKDeUewt_OcdAXseAYV_P9TpaAAAAABkI-HqAA"
-DATABASE_URI = "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://telegram:telegram@cluster0.ubgb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1001997863812
 ADMIN        = 6331847574
