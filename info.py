9988d1a44fa16227ab3909e31d293647eb4e2f9e
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-10-03 21:58:34 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 29942004
 API_HASH     = "ad92f01e4a90cddebbea0ad16fa23026"
-BOT_TOKEN    = "6765313019:AAHYMI7W7VZbq5iqm_pPlnlN0_P8wbFjK1s"
+BOT_TOKEN    = "6765313019:AAHYLXnKN_q5dhznb-4IuLddejkCFleIUg8"
 SESSION      = "BQHI4PQATVv5uF-p0ztSfnI2sjOk55qIZUIZ4kqSOBBZEr5ikUJGO-ngeMAMkdYVFcezIjxKZnz8OM0ZkF7oku0qbkqE_yukTp734ClmlqtbO6JqFtsJm2j5bVSK1iNZQxAo5Qyf-IlJydLBKYE8y-PmWzukYlJaGk5quYd2E7dPTnU9cv3bF14WBaCb8L2Sow_CP5E3rZcDzmJKiOcETLTuctXuTJTF2llZicgxMi-swnganyK08zf4vIzCi0mvX_rPLBYnUH78a_4jVUep7pA4ojWtcTlPLVVUm2N8nsmmmMH9d1cteDhRmApRHJmc6Q_GdKDeUewt_OcdAXseAYV_P9TpaAAAAABkI-HqAA"
 DATABASE_URI = "mongodb+srv://telegram:telegram@cluster0.ubgb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1001997863812
