de88b8ad8c4b029769e1bb412baef7979889c3bf
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-12-29 21:10:14 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 29942004
 API_HASH     = "ad92f01e4a90cddebbea0ad16fa23026"
 BOT_TOKEN    = "6765313019:AAHYLXnKN_q5dhznb-4IuLddejkCFleIUg8"
-SESSION      = "BQHI4PQAWmL8BzlCS5zz2VkCtBfrGGO_pR9M4up7OtSE0HGVvJGqkl0Hw8-brcScGBcFTv79LVctCe5UxWFObR603ay1e6NOhJQsFW4-4X6JEgk_iOSpiyHsD6iLMoFVDSaFXKKnBy-0VwfBgKIppQepmUSWU4dZTPzGjP2V3TwkiyhGZUleLXaCbMhhd2sJ8svGunxebqB35xxzQoNEH2Vqs4wkjSuyTIBpptdhw9U_0DT-dtt9JthxEmC1CiCDMjsuRdsR6oMOW2-XvA4FXBeYlMQ3Ee7wb-P_K_927ZIxqbLs08qt0pSjbQarvHvNA4tFi_kmB3iz-enYpOVLaCdX0Peu8QAAAABkI-HqAA"
-DATABASE_URI = "mongodb+srv://telegram:telegram@cluster0.ubgb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+SESSION      = "BQHI4PQAwPDmVNvj22GZNu_6POsG9NKmb2-_DMlrtwM_yL8bPCtPQydgECy6da-1C_v1dHzEjG3KaWqoJQPRKKeYDz7GawRlKP510hHYhBMacvnjugkkn67sFUyIOmxT5zsTaselih4VMkOol-Z7Mf09jh0FDhctTdkXkd70pLfj4Vf1R9ZFwCiF2CVn_XvgF8_5MByhY_bd0osQgLF-MtxPYhXsT5ZTr04CpAnGkE7jA7UpEevtmoP6bY1Lrp5YXF15gyurd1MFjXEQTZvQ5mraGFo1X5u6xe0TxarkWvbVf_0-nXnGpTOzbklXgrBZ5FELvwolBas-OgHbjs6gDAVL5sDZuAAAAABkI-HqAA"
+DATABASE_URI = "mongodb+srv://strong:strong@cluster0.ix7usa3.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001997863812
 ADMIN        = 6643562770
