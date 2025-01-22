645b602dd07e8472056a0284a1701a4f0e57a399
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-01-22 18:39:45 +0600

    Update info.py

--- info.py
+++ info.py
@@ -4,4 +4,4 @@ BOT_TOKEN    = "6765313019:AAHYLXnKN_q5dhznb-4IuLddejkCFleIUg8"
 SESSION      = "BQHI4PQAwPDmVNvj22GZNu_6POsG9NKmb2-_DMlrtwM_yL8bPCtPQydgECy6da-1C_v1dHzEjG3KaWqoJQPRKKeYDz7GawRlKP510hHYhBMacvnjugkkn67sFUyIOmxT5zsTaselih4VMkOol-Z7Mf09jh0FDhctTdkXkd70pLfj4Vf1R9ZFwCiF2CVn_XvgF8_5MByhY_bd0osQgLF-MtxPYhXsT5ZTr04CpAnGkE7jA7UpEevtmoP6bY1Lrp5YXF15gyurd1MFjXEQTZvQ5mraGFo1X5u6xe0TxarkWvbVf_0-nXnGpTOzbklXgrBZ5FELvwolBas-OgHbjs6gDAVL5sDZuAAAAABkI-HqAA"
 DATABASE_URI = "mongodb+srv://strong:strong@cluster0.ix7usa3.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1002497269320
-ADMIN        = 6643562770
+ADMIN        = 6605647659
