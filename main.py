b46048c588d7865a1662c57ad044219961c53b0e
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-01-26 17:38:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from client import Bot
+from app import run_bot  # Import the function to run the Pyrogram bot
 
-print("Bot Started 👍")
-Bot().run()
-
+if __name__ == "__main__":
+    print("Bot and Flask Server Started 👍")
+    
+    # Start the bot in a separate thread so Flask can run in parallel
+    run_bot()  # Run the Pyrogram bot asynchronously
